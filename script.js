f95d2ce4b3abdce03696a8f47f0325ff64af6a3c
Benjamin Abbott <ben_abbott@outlook.com>
2023-02-08 13:17:44 +0000

    add random button and color squares match the background when random button is used

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+let inputColor1 = document.querySelector(".color1");
+let inputColor2 = document.querySelector(".color2");
 let css = document.querySelector("h3");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
+let button = document.querySelector("button");
+css.textContent = getComputedStyle(body).getPropertyValue('--start-background');
 
-function setGradient() {
-	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
+const changeBackground = () => {
+	body.style.background = "linear-gradient( to right, " + inputColor1.value + ", " + inputColor2.value + ")";
 	css.textContent = body.style.background + ";";
+};
+
+const randomHex = () => {
+	return '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'); // found on stack overflow
 }
 
 
-color1.addEventListener("input", setGradient);
-
-color2.addEventListener("input", setGradient);
+inputColor1.addEventListener("input", changeBackground);
+inputColor2.addEventListener("input", changeBackground);
+button.addEventListener("click", () => {
+	inputColor1.value = randomHex();
+	inputColor2.value = randomHex();
+	body.style.background = changeBackground();
+});
